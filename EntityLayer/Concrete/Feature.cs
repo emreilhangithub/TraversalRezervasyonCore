b8ab00260bf93a5c7615b1bacde2981901686080
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-11-05 01:53:27 +0300

    Entitylerin Tamamladım.

--- EntityLayer/Concrete/Feature.cs
+++ EntityLayer/Concrete/Feature.cs
@@ -7,15 +7,12 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class About
+    public class Feature
     {
-        [Key]
-        public int AboutID { get; set; }
+        public int FeatureID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Image1 { get; set; }
-        public string Title2 { get; set; }
-        public string Description2 { get; set; }
-        public string Status { get; set; }
+        public string Image { get; set; }
+        public bool Status { get; set; }
     }
 }
