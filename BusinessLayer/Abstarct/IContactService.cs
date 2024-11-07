72ac8082ca9701572279b48bc6e7cb21aa2eb6fe
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-11-08 01:15:19 +0300

    Business Katmanında Interface ve Sınıfları oluşturuldum.

---
+++ BusinessLayer/Abstarct/IContactService.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstarct
+{
+    public interface IContactService : IGenericService<Contact>
+    {
+    }
+}
